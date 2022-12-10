a5f582bc9c8a125475fba2db35a0c92dc0cadfe4
himanshuchauhan59 <104261570+himanshuchauhan59@users.noreply.github.com>
2022-12-10 23:06:44 +0530

    save

--- src/Component/Navbar.js
+++ src/Component/Navbar.js
@@ -1,34 +1,49 @@
-import React from "react";
-
-function Navbar(props) {
+import React, { Component } from "react";
+import Form from "./Contact";
+import Home from "./Home";
+import About from "./About";
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Link
+} from 'react-router-dom';
+export default function Navbar(props) {
     return (
-        <> 
-            <div>
-                <nav className="navbar navbar-expand-lg bg-light">
-                    <div className="container-fluid">
-                        <a className="navbar-brand" href="navbar.html">{props.Navbarname}</a>
-                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                            <span className="navbar-toggler-icon"></span>
-                        </button>
-                        <div className="collapse navbar-collapse" id="navbarNav">
-                            <ul className="navbar-nav">
-                                <li className="nav-item">
-                                    <a className="nav-link active" href="navbar.html" aria-current="page" >{props.name}</a>
-                                </li>
-                                <li className="nav-item">
-                                    <a className="nav-link" href="navbar.html">{props.author.name}</a>
-                                </li>
-                                <li className="nav-item">
-                                    <a className="nav-link" href="navbar.html">{DateTime}</a>
-                                </li>
+        <>
+            <Router>
+                <div>
+                    <nav className="navbar navbar-expand-lg navbar-dark bg-dark my-1">
+                        <div className="container-fluid">
+                            <a className="navbar-brand" >{props.name}</a>
+                            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                                <span className="navbar-toggler-icon"></span>
+                            </button>
+                            <div className="collapse navbar-collapse" id="navbarText">
+                                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
 
-                            </ul>
+                                    <li className="nav-item">
+                                        <Link to="/" className="nav-link active">Home</Link>
+                                    </li>
+                                    <li className="nav-item">
+                                        <Link to="/Form" className="nav-link ">contact</Link>
+                                    </li>
+                                    <li className="nav-item">
+                                        <Link to="/About" className="nav-link ">About</Link>
+                                    </li>
+                                    
+                                </ul>
+
+                                <span className="navbar-text text-light">
+                                    {props.date.toLocaleTimeString()}
+                                </span>
+                            </div>
                         </div>
-                    </div>
-                </nav>
-            </div>
+                    </nav>
+                    
+                </div>
+            </Router>
         </>
+
     );
 }
-
-export default Navbar
